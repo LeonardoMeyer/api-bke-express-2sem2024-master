d91ec86e46e2f8da4987e2b98e6a4a84a72fda81
LeonardoMeyer <leonardo.meyerr@outlook.com>
2024-09-03 15:42:03 -0300

    pourrarrarra

--- src/routers/accountRouter.js
+++ src/routers/accountRouter.js
@@ -1,6 +1,6 @@
 import express from 'express'
 import createController from '../controllers/account/createController.js'
-import getController from '../controllers/account/getController.js';
+import getController from '../controllers/account/getByIdController.js';
 import listController from '../controllers/account/listController.js'
 import updateController from '../controllers/account/updateController.js'
 import removeController from '../controllers/account/removeController.js'
