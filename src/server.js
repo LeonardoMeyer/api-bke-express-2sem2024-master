78792e9d8a94aecd1d7f40441e7b0a9235bb8e8f
LeonardoMeyer <leonardo.meyerr@outlook.com>
2024-09-03 14:58:33 -0300

    add prisma local-production

---
+++ src/server.js
@@ -0,0 +1,19 @@
+import express from 'express'
+import authRouter from './routers/authRouter.js'
+import accountRouter from './routers/accountRouter.js'
+import { ENVIRONMENT, PORT, HOST } from './config.js'
+
+const app = express()
+app.use(cors({
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+}))
+app.get('/', (req, res) => {res.json({message: "Bem-vindo a API!"})})
+
+app.use('/auth', authRouter)
+app.use('/account', accountRouter)
+
+app.listen(PORT, () => {
+    console.log(`Servidor Rodando no ambiente ${ENVIRONMENT} em ${ ENVIRONMENT == 'production' ? HOST : HOST+':'+PORT }`)
+})
